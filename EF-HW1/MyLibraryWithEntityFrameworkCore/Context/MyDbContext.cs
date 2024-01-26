6323e04d97a4158305bb81fc03544ef87984bbbc
Sanan Akhverdiev <120374801+sakhverdiev@users.noreply.github.com>
2024-01-26 12:15:05 +0400

    first commit

---
+++ EF-HW1/MyLibraryWithEntityFrameworkCore/Context/MyDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLibraryWithEntityFrameworkCore.Models;
+
+
+namespace MyLibraryWithEntityFrameworkCore.Context 
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Author>? Authors { get; set; }
+        public DbSet<Book>? Books { get; set; }
+        public DbSet<BookType>? BookTypes { get; set; }
+        public DbSet<Operation>? Operations { get; set; }
+        public DbSet<Student>? Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("DESKTOP-KOTE4M2\\SQLEXPRESS02;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Operation>().Ignore("Id");
+            modelBuilder.Entity<Operation>().HasKey("StudentId", "BookId");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EF-HW1/MyLibraryWithEntityFrameworkCore/Program.cs
@@ -0,0 +1,10 @@
+﻿using MyLibraryWithEntityFrameworkCore.Context;
+using MyLibraryWithEntityFrameworkCore.Models;
+
+
+using (var context = new MyDbContext())
+{
+    context.Authors?.Add(new Author { FirstName = "ABC", LastName = "ZXC" });
+    context.SaveChanges();
+}
+Console.WriteLine("The author added"); 
